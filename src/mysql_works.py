781d4f39c418ef1f2a2e259042b4f63a09bd66da
C. Giray <cihangiray.oner@gmail.com>
2024-05-31 15:08:57 +0000

    mysql_file_changed

--- src/mysql_works.py
+++ src/mysql_works.py
@@ -9,9 +9,7 @@ import numpy as np
 import re
 import json
 
-# mydb = mysql.connector.connect(user = os.environ['MYSQLSERVER_USER'], password = os.environ['MYSQLSERVER_PASS'],
-#                               host ='localhost',
-#                               database ='sakila')
+
 print("davar")
 
 df = dat.df_read_aws('merkle-de-interview-case-study/de','item.csv')
@@ -23,9 +21,3 @@ for key,value in functions_df_names.items():
     vars()[key] = value(df)
 
 dat.save_dataframe_excel(globals(),f"analysis_dataset_{date.today()}")
-# mycursor = mydb.cursor()
-
-# user = os.environ['MYSQLSERVER_USER']
-# password = os.environ['MYSQLSERVER_PASS']
-
-# dat.multiple_dataset_apply_sql.multiple_dataset_apply_containing_cols_mysql('localhost','root','Dingilaz55!','sakila')
