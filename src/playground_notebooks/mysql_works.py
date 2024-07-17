a996017f6fa103e5ebda17bb3e290f1fb77333be
C. Giray <cihangiray.oner@gmail.com>
2024-07-17 16:08:42 +0200

    linter_set_up

--- src/playground_notebooks/mysql_works.py
+++ src/playground_notebooks/mysql_works.py
@@ -1,23 +1,16 @@
-import mysql.connector
+from datetime import date
+import sys
+sys.path.insert(0, 'C:/Users/Lenovo/Desktop/exam_eti/containerized_tool/data_analysis_tool/src')
 import dat
-import os
-from datetime import datetime, timedelta, date
-import openpyxl
-import xlsxwriter
-import pandas as pd
-import numpy as np
-import re
-import json
-
 
 print("davar")
 
-df = dat.df_read_aws('merkle-de-interview-case-study/de','item.csv')
+df = dat.df_read_aws('merkle-de-interview-case-study/de', 'item.csv')
 
 functions_df_names = dat.analysis_dict()
 
-for key,value in functions_df_names.items():
+for key, value in functions_df_names.items():
     # Creating dataframes defined on analysis_dict.py file
     vars()[key] = value(df)
 
-dat.save_dataframe_excel(globals(),f"analysis_dataset_{date.today()}")
+dat.save_dataframe_excel(globals(), f"analysis_dataset_{date.today()}")
