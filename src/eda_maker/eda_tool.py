8334c8a4cbac044484277511c2da824ed9241581
C. Giray <cihangiray.oner@gmail.com>
2024-07-18 16:47:39 +0200

    eda_notebook_development

--- src/eda_maker/eda_tool.py
+++ src/eda_maker/eda_tool.py
@@ -1,33 +1,85 @@
 import sys
-sys.path.insert(0, 'C:/Users/Lenovo/Desktop/exam_eti/containerized_tool/data_analysis_tool/src')
+sys.path.insert(
+    0, 'C:/Users/Lenovo/Desktop/exam_eti/containerized_tool/data_analysis_tool/src')
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
 import numpy as np
 import pandas as pd
 import dat
 import mysql.connector
 import mysql_analyzer
 from collections import defaultdict
+from mysql.connector.errors import Error
+from mysql.connector import errorcode
 import json
 import re
 import os
 from datetime import datetime, timedelta, date
 from dataclasses import dataclass, field
 
+
+# import mysql.connector.errors as err
+
+
 @dataclass
 class EdaHelper:
     '''Class to apply exploratory data analysis activities to datasets'''
-    host : str
-    user : str
-    password : str
-    database : str
-    db : str = field(init=False)
+    host: str
+    user: str
+    password: str
+    database: str
+    db: str = field(init=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         # Creating connection string
-        self.db=mysql.connector.connect(host = str(self.host), user = str(self.user), password = str(self.password), database = str(self.database))
+        try:
+            self.db = mysql.connector.connect(host=str(self.host), user=str(
+                self.user), password=str(self.password), database=str(self.database))
+        except mysql.connector.ProgrammingError as e:
+            raise ValueError('Wrong database name') from e
+        except mysql.connector.DatabaseError as err:
+            raise ValueError('Wrong server name') from err
 
-    def sql_command(self,sql_command : str = None):
-        '''Runnint SQL command passed to function and fetch all brings results'''
-        cursor=self.db.cursor()
+    def sql_query(self, sql_command: str = None) -> list:
+        '''Running SQL command passed to function and fetch all brings results'''
+        cursor = self.db.cursor()
         cursor.execute(sql_command)
-        # myresult = cursor.fetchall()
-        return cursor.fetchall()
+        res = cursor.fetchall()
+        cursor.close()
+        return res
+
+    def sql_query_to_pandas(self, sql_command: str = None) -> pd.DataFrame:
+        '''Running SQL command parsing query results saving into dataframe'''
+        df = pd.read_sql_query(f'{sql_command}', con=self.db)
+        return df
+
+    def filter_numeric_cols(self, sql_command: str = None) -> list:
+        '''Returns numeric columns of dataframe into a list'''
+        num_cols = list(pd.read_sql_query(
+            f'{sql_command}', con=self.db)._get_numeric_data().columns)
+        return num_cols
+
+    def filter_categorical_cols(self, sql_command: str = None) -> list:
+        '''Returns categorical columns of dataframe into a list'''
+        df_cols = list(self.sql_query_to_pandas(f'{sql_command}').columns)
+        cat_cols = [
+            x for x in df_cols if x not in self.filter_numeric_cols(f'{sql_command}')]
+        return cat_cols
+
+    def desc_uni_num_feature(self, feature_name: str = None, sql_command: str = None, bins:  int = 30, edgecolor: str = 'k', **kwargs) -> list:
+        '''Returns matplotlib graph of selected columns hist dist. and describe values'''
+        fig, ax = plt.subplots(figsize=(8, 4))
+        df = pd.read_sql_query(
+            f'{sql_command}', con=self.db)[feature_name].hist(bins=bins, edgecolor=edgecolor, ax=ax, **kwargs)
+        ax.set_title(feature_name, size=15)
+        plt.figtext(1.15, 0.15, pd.read_sql_query(
+            f'{sql_command}', con=self.db)[feature_name].describe().round(2), size=17)
+
+    def desc_uni_cat_feature(self, feature_name: str = None, sql_command: str = None) -> list:
+        count = pd.read_sql_query(
+            f'{sql_command}', con=self.db)[feature_name].value_counts()
+        percent = 100*pd.read_sql_query(
+            f'{sql_command}', con=self.db)[feature_name].value_counts(normalize=True)
+        df = pd.DataFrame({'count':count, 'percent':percent.round(1)})
+        return df
--- src/mysql_analyzer/mysql_pandas.py
+++ src/mysql_analyzer/mysql_pandas.py
@@ -1,42 +1,44 @@
+import time
+import random
+from faker import Faker
+from mysql.connector.errors import Error
+from datetime import datetime, timedelta, date
+import mysql.connector
+import os
+import re
+import json
+from collections import defaultdict
+import dat
+import pandas as pd
 import numpy as np
 import sys
 # sys.path.insert(0, 'C:/Users/Lenovo/Desktop/exam_eti/containerized_tool/data_analysis_tool')
 sys.path.insert(0, '/src/src')
-import pandas as pd
-import dat
-from collections import defaultdict
-import json
-import re
-import os
-import mysql.connector
-from datetime import datetime, timedelta, date
-from mysql.connector.errors import Error
-from faker import Faker
-import random
-import time
 # import mysql_analyzer
 
 
 class mysql_profiler:
     '''Class to create quick profiling of tables located on mysql server'''
 
-    def __init__(self,host,user,password,database) -> None:
+    def __init__(self, host, user, password, database) -> None:
         self.host = host
         self.user = user
         self.password = password
         self.database = database
-        
+
     def multiple_dataset_apply_mysql(self):
         ''' This function reads multiple tables from connected database as parameter 
         then runs all functions on dat package to read table '''
 
-        #Regex pattern for date columns
+        # Regex pattern for date columns
         pattern_d = re.compile(r"[0-9]{4}.[0-9]{2}.[0-9]{2}.*", re.IGNORECASE)
-        pattern_d_alt = re.compile(r"[0-9]{2}.[0-9]{2}.[0-9]{4}.*", re.IGNORECASE)
+        pattern_d_alt = re.compile(
+            r"[0-9]{2}.[0-9]{2}.[0-9]{4}.*", re.IGNORECASE)
 
         # Creating connection string
-        db=mysql.connector.connect(host = str(self.host), user = str(self.user), password = str(self.password), database = str(self.database))
-        cursor=db.cursor()
+        db = mysql.connector.connect(host=str(self.host), user=str(
+            self.user), password=str(self.password), database=str(self.database))
+        cursor = db.cursor()
         cursor.execute("SHOW TABLES")
         myresult = cursor.fetchall()
 
@@ -47,7 +49,8 @@ class mysql_profiler:
         dataframes_dict = {}
 
         # Creating working directory for daily partitioning
-        dir = os.path.join("C:\\", "Users\Lenovo\Desktop\exam_eti\containerized_tool\data_analysis_tool", f'{date.today()}')
+        dir = os.path.join(
+            "C:\\", "Users\Lenovo\Desktop\exam_eti\containerized_tool\data_analysis_tool", f'{date.today()}')
         if not os.path.exists(dir):
             os.mkdir(dir)
         os.chdir(dir)
@@ -57,16 +60,17 @@ class mysql_profiler:
             df = pd.read_sql(f'SELECT * FROM {f}', con=db)
             # Creating a dict consisting of all dataframes
             if df.size != 0:
-                dataframes_dict[f] = df  
+                dataframes_dict[f] = df
             # Applying methods to dataframes defined on analysis_dict.py file
-            for key,value in dat.analysis_dict().items():
+            for key, value in dat.analysis_dict().items():
                 if df.size != 0:
                     vars()[key] = value(df)
-                    dat.save_dataframe_excel(vars(),f"analysis_{f}_{date.today()}")
+                    dat.save_dataframe_excel(
+                        vars(), f"analysis_{f}_{date.today()}")
                 else:
-                    dat.save_dataframe_excel(df,f"analysis_{f}_{date.today()}")
+                    dat.save_dataframe_excel(
+                        df, f"analysis_{f}_{date.today()}")
         return dataframes_dict
-
 
     def multiple_dataset_apply_containing_cols_mysql(self):
         '''Detects columns that contains each other on entire database and saves as output file'''
@@ -76,33 +80,38 @@ class mysql_profiler:
         # dataframes_dict = self.multiple_dataset_apply_mysql(self.host, self.user, self.password, self.database)
         dataframes_dict = self.multiple_dataset_apply_mysql()
         # Containing columns operation
-        for key,value in dataframes_dict.items():
-            for key_col,columnData in value.items():
-                for key_1,value_1 in dataframes_dict.items():
-                    for key_col_1,colondata in value_1.items():
-                        if columnData.isin(colondata).all() == True and key_col == key_col_1 and key != key_1: # and "id" not in key_col and "Id" not in key_col_1
-                            d = {"table_1" : [key], "col_1" : [key_col], "table_2" : [key_1], "col_2" : [key_col_1]}
+        for key, value in dataframes_dict.items():
+            for key_col, columnData in value.items():
+                for key_1, value_1 in dataframes_dict.items():
+                    for key_col_1, colondata in value_1.items():
+                        # and "id" not in key_col and "Id" not in key_col_1
+                        if columnData.isin(colondata).all() == True and key_col == key_col_1 and key != key_1:
+                            d = {"table_1": [key], "col_1": [
+                                key_col], "table_2": [key_1], "col_2": [key_col_1]}
                             df_output = pd.DataFrame(data=d)
                             frames.append(df_output)
                             global df_ultimate
                             df_ultimate = pd.concat(frames)
         # Creating working directory for daily partitioning of output files
-        dir = os.path.join("C:\\", "Users\Lenovo\Desktop\exam_eti\containerized_tool\data_analysis_tool", f'{date.today()}')
+        dir = os.path.join(
+            "C:\\", "Users\Lenovo\Desktop\exam_eti\containerized_tool\data_analysis_tool", f'{date.today()}')
         if not os.path.exists(dir):
             os.mkdir(dir)
         os.chdir(dir)
         # Writing to xlsx file
-        writer = pd.ExcelWriter(f"containing_cols_{date.today()}.xlsx", engine="xlsxwriter")
-        df_ultimate.to_excel(writer,sheet_name="containing_cols")
+        writer = pd.ExcelWriter(
+            f"containing_cols_{date.today()}.xlsx", engine="xlsxwriter")
+        df_ultimate.to_excel(writer, sheet_name="containing_cols")
         writer.close()
         return "output files saved for containing columns"
-    
-    def find_value_mysql(self,value:str):
+
+    def find_value_mysql(self, value: str):
         ''' This function reads multiple tables from connected database 
         then find values searched in database tables saves into an xlxs'''
         # mysql connection cursor
-        db=mysql.connector.connect(host = str(self.host), user = str(self.user), password = str(self.password), database = str(self.database))
-        cursor=db.cursor()
+        db = mysql.connector.connect(host=str(self.host), user=str(
+            self.user), password=str(self.password), database=str(self.database))
+        cursor = db.cursor()
         cursor.execute("SHOW TABLES")
         myresult = cursor.fetchall()
         # saving all tables of database into a list
@@ -117,53 +126,57 @@ class mysql_profiler:
             # Creating output dataframe
             df_check = pd.DataFrame([value], columns=["value"])
             # Detect values from read table and save into frames list
-            for k,v in df.items():
-                for k1,v1 in df_check.items():
+            for k, v in df.items():
+                for k1, v1 in df_check.items():
                     if v1.isin(v).any():
-                        d = {"column" : [k], "table" : [f], "value" : [value]}
+                        d = {"column": [k], "table": [f], "value": [value]}
                         frames.append(d)
-        # Changing WD for save output 
-        dir = os.path.join("C:\\", "Users\Lenovo\Desktop\exam_eti\containerized_tool\data_analysis_tool", f'{date.today()}')
+        # Changing WD for save output
+        dir = os.path.join(
+            "C:\\", "Users\Lenovo\Desktop\exam_eti\containerized_tool\data_analysis_tool", f'{date.today()}')
         if not os.path.exists(dir):
             os.mkdir(dir)
         os.chdir(dir)
-        # Saving output dataframe 
-        writer = pd.ExcelWriter(f"find_value_{value}_{date.today()}.xlsx", engine="xlsxwriter")
+        # Saving output dataframe
+        writer = pd.ExcelWriter(
+            f"find_value_{value}_{date.today()}.xlsx", engine="xlsxwriter")
         df_output = pd.DataFrame(frames)
-        df_output.to_excel(writer,sheet_name="found_value")
+        df_output.to_excel(writer, sheet_name="found_value")
         writer.close()
         return "value find table saved"
-    
 
-    def multiple_dataset_apply_mysql_query(self,sql_command:str):
+    def multiple_dataset_apply_mysql_query(self, sql_command: str):
         ''' This function gets sql statement as text input and runs it through connected 
          MYSQL db '''
         # Creating connection string and running sql command
-        db=mysql.connector.connect(host = str(self.host), user = str(self.user), password = str(self.password), database = str(self.database))
-        cursor=db.cursor()
+        db = mysql.connector.connect(host=str(self.host), user=str(
+            self.user), password=str(self.password), database=str(self.database))
+        cursor = db.cursor()
         cursor.execute(f"{sql_command}")
         myresult = cursor.fetchall()
         return myresult
-    
-    def multiple_dataset_apply_mysql_insert(self,sql_command:str):
+
+    def multiple_dataset_apply_mysql_insert(self, sql_command: str):
         ''' This function gets sql insert statement as text input and 
         runs it through connected 
         MYSQL db '''
         # Creating connection string and running sql command
-        db=mysql.connector.connect(host = str(self.host), user = str(self.user), password = str(self.password), database = str(self.database))
-        cursor=db.cursor()
+        db = mysql.connector.connect(host=str(self.host), user=str(
+            self.user), password=str(self.password), database=str(self.database))
+        cursor = db.cursor()
         cursor.execute(f"{sql_command}")
         db.commit()
         cursor.close()
         db.close()
         return 'INSERT INTO STATEMENT EXECUTED'
-    
-    def fake_record_creator_sakila(self,sql_command='SELECT max(address_id) FROM address'):
+
+    def fake_record_creator_sakila(self, sql_command='SELECT max(address_id) FROM address'):
         ''' This method creates 10 fake records on customers and address table of 
          Sakila schema '''
         # Creating connection string and running sql command
         fake = Faker()
-        last_address_id = self.multiple_dataset_apply_mysql_query(f'{sql_command}')
+        last_address_id = self.multiple_dataset_apply_mysql_query(
+            f'{sql_command}')
         id = int(str(last_address_id[0]).strip("(,)"))
         counter = 0
         while counter < 20:
@@ -192,6 +205,7 @@ class mysql_profiler:
             counter += 1
             print(id)
             # Wait a few seconds before sending another transaction
-            time.sleep(random.randrange(5,10))
+            time.sleep(random.randrange(5, 10))
+
 
 print("working")
